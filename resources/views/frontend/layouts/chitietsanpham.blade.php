959b3bda369b9ad09f72085e4f4f9fe28f9f14be
Trần Mạnh Dũng <dungbk0902@gmail.com>
2017-04-29 00:31:27 +0700

    dungcomit

--- resources/views/frontend/layouts/chitietsanpham.blade.php
+++ resources/views/frontend/layouts/chitietsanpham.blade.php
@@ -10,59 +10,9 @@
 <body>
 <div class="container warraper">
     <div id="custom_carousel" class="carousel slide" data-ride="carousel" data-interval="4000">
-        <!-- Wrapper for slides -->
-        <div class="carousel-inner">
-
-            @if( is_object($post) && isset($post->id))
-                <?php
-                $i = 0;
-                ?>
-
-                @foreach($images as $image)
-                    @if ($i == 0)
-                        <div class="item active">
-                            <div class="container-fluid">
-                                <div class="row">
-                                    <div class="top col-md-12 col-xs-12"><img
-                                                src="{!! asset($image->path) !!}"
-                                                class="img-responsive"></div>
-                                </div>
-                            </div>
-                        </div>
-                        <?php
-                        $i = 1;
-                        ?>
-                    @else
-                        <div class="item ">
-                            <div class="container-fluid">
-                                <div class="row">
-                                    <div class="top col-md-12 col-xs-12"><img src="{!! asset($image->path) !!}"
-                                                                              class="img-responsive"></div>
-                                </div>
-                            </div>
-                        </div>
-                @endif
-            @endforeach
-        @endif
-
-        <!-- End Item -->
-        </div>
-        <a data-slide="prev" href="#custom_carousel" class="izq carousel-control">‹</a>
-        <a data-slide="next" href="#custom_carousel" class="der carousel-control">›</a>
-        <!-- End Carousel Inner -->
-        <div class="controls draggable ui-widget-content col-md-12 col-xs-12">
-            <ul class="nav ui-widget-header">
-                <?php $j = 1; ?>
-                @foreach($images as $image)
-                    <li data-target="#custom_carousel" data-slide-to="0" class="active"><a href="#"><img
-                                    src="{!! asset($image->path) !!}">
-                            <small>Ảnh {!! $j !!}</small>
-                            <?php $j++; ?>
-                        </a></li>
-                @endforeach
-
-            </ul>
-        </div>
+        <!-- Goi 1 trong 2 thang 2 day -->
+        @include('layouts.image_360')
+        @include('layouts.slide_chitiet')
     </div>
     <div class="title">
         <h3><b>{!! $post->title !!}</b></h3>
---
+++ resources/views/frontend/layouts/image_360.blade.php
@@ -0,0 +1,7 @@
+
+<div class="container" >
+    <div class="iframe-slide">
+    <iframe src="http://batdongsan360.ga/">
+    </iframe>
+    </div>
+</div>
