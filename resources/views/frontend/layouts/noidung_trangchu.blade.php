bb9cd5909cd3b3dbaafecfbeec7a101fd010c9ea
thanhtn <nhatthanh28594@gmail.com>
2017-05-23 00:01:46 +0700

    ThanhTN fix layout conflict

--- resources/views/frontend/layouts/noidung_trangchu.blade.php
+++ resources/views/frontend/layouts/noidung_trangchu.blade.php
@@ -1,41 +1,57 @@
 <div class="row">
     <div class="col-xs-12 col-sm-6 col-md-4 section">
-        <a href=""><img class="trangchu-item" src="public/images/pic10.jpg"></a>
-        <h3 class="title">Bất Động Sản Hà Nội</h3>
+        <div class="item-index">
+            <a href=""><img class="trangchu-item" src="public/images/pic10.jpg"></a>
+            <h3 class="title">Bất Động Sản Hà Nội</h3>
+        </div>
     </div>
     <div class="col-xs-12 col-sm-6 col-md-4 section">
-        <a href=""><img class="trangchu-item" src="public/images/pic9.jpg"></a>
-        <h3 class="title">Bất Động Sản Hà Nội</h3>
+        <div class="item-index">
+            <a href=""><img class="trangchu-item" src="public/images/pic9.jpg"></a>
+            <h3 class="title">Bất Động Sản Hà Nội</h3>
+        </div>
 
     </div>
     <div class="col-xs-12 col-sm-6 col-md-4 section">
-        <a href=""><img class="trangchu-item" src="public/images/pic10.jpg"></a>
-        <h3 class="title">Bất Động Sản Hà Nội</h3>
+        <div class="item-index">
+            <a href=""><img class="trangchu-item" src="public/images/pic10.jpg"></a>
+            <h3 class="title">Bất Động Sản Hà Nội</h3>
+        </div>
     </div>
-	<div class="col-xs-12 col-sm-6 col-md-4 section">
+    <div class="col-xs-12 col-sm-6 col-md-4 section">
         <a href=""><img class="trangchu-item" src="public/images/pic10.jpg"></a>
         <h3 class="title">Bất Động Sản Hà Nội</h3>
     </div>
     <div class="col-xs-12 col-sm-6 col-md-4 section">
-        <a href=""><img class="trangchu-item" src="public/images/pic9.jpg"></a>
-        <h3 class="title">Bất Động Sản Hà Nội</h3>
+        <div class="item-index">
+            <a href=""><img class="trangchu-item" src="public/images/pic9.jpg"></a>
+            <h3 class="title">Bất Động Sản Hà Nội</h3>
+        </div>
 
     </div>
     <div class="col-xs-12 col-sm-6 col-md-4 section">
-        <a href=""><img class="trangchu-item" src="public/images/pic10.jpg"></a>
-        <h3 class="title">Bất Động Sản Hà Nội</h3>
-    </div>
-	<div class="col-xs-12 col-sm-6 col-md-4 section">
-        <a href=""><img class="trangchu-item" src="public/images/pic10.jpg"></a>
-        <h3 class="title">Bất Động Sản Hà Nội</h3>
+        <div class="item-index">
+            <a href=""><img class="trangchu-item" src="public/images/pic10.jpg"></a>
+            <h3 class="title">Bất Động Sản Hà Nội</h3>
+        </div>
     </div>
     <div class="col-xs-12 col-sm-6 col-md-4 section">
-        <a href=""><img class="trangchu-item" src="public/images/pic9.jpg"></a>
-        <h3 class="title">Bất Động Sản Hà Nội</h3>
+        <div class="item-index">
+            <a href=""><img class="trangchu-item" src="public/images/pic10.jpg"></a>
+            <h3 class="title">Bất Động Sản Hà Nội</h3>
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-6 col-md-4 section">
+        <div class="item-index">
+            <a href=""><img class="trangchu-item" src="public/images/pic9.jpg"></a>
+            <h3 class="title">Bất Động Sản Hà Nội</h3>
+        </div>
 
     </div>
     <div class="col-xs-12 col-sm-6 col-md-4 section">
-        <a href=""><img class="trangchu-item" src="public/images/pic10.jpg"></a>
-        <h3 class="title">Bất Động Sản Hà Nội</h3>
+        <div class="item-index">
+            <a href=""><img class="trangchu-item" src="public/images/pic10.jpg"></a>
+            <h3 class="title">Bất Động Sản Hà Nội</h3>
+        </div>
     </div>
 </div>
